98ef312ae185355ac183f32d3246ae86241b786b
Uriasej <uriasej@owensborocatholic.us>
2023-02-22 20:52:45 -0600

    Added Molten Obsidian Pickaxe and texture and recipe

--- src/main/java/com/confusingfool/moltenobsidiantools/item/ModItems.java
+++ src/main/java/com/confusingfool/moltenobsidiantools/item/ModItems.java
@@ -2,7 +2,7 @@ package com.confusingfool.moltenobsidiantools.item;
 
 import com.confusingfool.moltenobsidiantools.MoltenObsidianTools;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.PickaxeItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,6 +15,10 @@ public class ModItems
 
     public static final RegistryObject<Item> MOLTEN_OBSIDIAN = ITEMS.register("molten_obsidian",
             () -> new Item(new Item.Properties().tab(ModItemGroup.MOLTEN_OBSIDIAN_TOOLS).stacksTo(1)));
+
+    public static final RegistryObject<Item> MOLTEN_OBSIDIAN_PICKAXE = ITEMS.register("molten_obsidian_pickaxe",
+            () -> new PickaxeItem(ModItemTier.MOLTEN, 4, -1f,
+                    new Item.Properties().tab(ModItemGroup.MOLTEN_OBSIDIAN_TOOLS)));
 
 
     public static void register(IEventBus eventBus)
