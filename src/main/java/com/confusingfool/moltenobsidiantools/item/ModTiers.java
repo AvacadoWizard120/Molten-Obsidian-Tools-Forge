112000d29fc9f561a32cf3e4a6b257059893b3c9
Uriasej <uriasej@owensborocatholic.us>
2024-03-11 15:50:28 -0500

    1.19.4 added in textures and modified the speed of the reinforced obsidian tier

--- src/main/java/com/confusingfool/moltenobsidiantools/item/ModTiers.java
+++ src/main/java/com/confusingfool/moltenobsidiantools/item/ModTiers.java
@@ -9,6 +9,6 @@ public class ModTiers
     public static final ForgeTier MOLTEN = new ForgeTier(3, 1900, 9.4F, 3.5F, 3, Tags.Blocks.NEEDS_GOLD_TOOL,
             () -> Ingredient.of(ModItems.MOLTEN_OBSIDIAN.get()));
 
-    public static final ForgeTier REINFORCED = new ForgeTier(4, 3000, 11F, 4.0F, 20, Tags.Blocks.NEEDS_NETHERITE_TOOL,
+    public static final ForgeTier REINFORCED = new ForgeTier(4, 3000, 20F, 4.0F, 20, Tags.Blocks.NEEDS_NETHERITE_TOOL,
             () -> Ingredient.of(ModItems.REINFORCED_MOLTEN_OBSIDIAN.get()));
 }
