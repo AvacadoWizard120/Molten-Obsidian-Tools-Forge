99578fb68dca49b0851beb7d4c03adee7cde040e
Uriasej <uriasej@owensborocatholic.us>
2024-05-01 14:30:32 -0500

    Updated Tiers to 1.20.6

--- src/main/java/com/confusingfool/moltenobsidiantools/item/ModTiers.java
+++ src/main/java/com/confusingfool/moltenobsidiantools/item/ModTiers.java
@@ -1,14 +1,22 @@
 package com.confusingfool.moltenobsidiantools.item;
 
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.common.ForgeTier;
 import net.minecraftforge.common.Tags;
 
 public class ModTiers
 {
-    public static final ForgeTier MOLTEN = new ForgeTier(3, 1900, 9.4F, 3.5F, 3, Tags.Blocks.NEEDS_GOLD_TOOL,
-            () -> Ingredient.of(ModItems.MOLTEN_OBSIDIAN.get()));
+//    public static final ForgeTier MOLTEN = new ForgeTier(3, 1900, 9.4F, 3.5F, 3, Tags.Blocks.NEEDS_GOLD_TOOL,
+//            () -> Ingredient.of(ModItems.MOLTEN_OBSIDIAN.get()));
 
-    public static final ForgeTier REINFORCED = new ForgeTier(4, 3000, 20F, 4.0F, 20, Tags.Blocks.NEEDS_NETHERITE_TOOL,
-            () -> Ingredient.of(ModItems.REINFORCED_MOLTEN_OBSIDIAN.get()));
+    public static final ForgeTier MOLTEN = new ForgeTier(1900, 9.4F, 3.5F, 3, Tags.Blocks.NEEDS_GOLD_TOOL,
+            () -> Ingredient.of(ModItems.MOLTEN_OBSIDIAN.get()), BlockTags.INCORRECT_FOR_DIAMOND_TOOL);
+
+//    public static final ForgeTier REINFORCED = new ForgeTier(4, 3000, 20F, 4.0F, 20, Tags.Blocks.NEEDS_NETHERITE_TOOL,
+//            () -> Ingredient.of(ModItems.REINFORCED_MOLTEN_OBSIDIAN.get()));
+
+    public static final ForgeTier REINFORCED = new ForgeTier(3000, 20F, 4.0F, 20, Tags.Blocks.NEEDS_NETHERITE_TOOL,
+            () -> Ingredient.of(ModItems.REINFORCED_MOLTEN_OBSIDIAN.get()), BlockTags.INCORRECT_FOR_NETHERITE_TOOL);
 }
